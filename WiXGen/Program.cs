1fb19a3b271982344276c5691da41a18979cb755
Junian Triajianto <author@junian.net>
2019-03-06 05:06:06 +0700

    Init new project

---
+++ WiXGen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiXGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

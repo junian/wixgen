bf55718127898f425c9d193ceeba0ae16f1bdada
Junian Triajianto <author@junian.net>
2019-03-06 05:17:15 +0700

    Add program to generate WiX xml

--- WiXGen/Program.cs
+++ WiXGen/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace WiXGen
 {
@@ -6,7 +8,50 @@ namespace WiXGen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 1 || args.Length > 1)
+                return;
+
+            var directory = args[0];
+
+            if (!Directory.Exists(directory))
+            {
+                Console.WriteLine("Directory doesn't exist.");
+                return;
+            }
+
+            var files = Directory.GetFiles(directory);
+            foreach (var file in files)
+            {
+                var filename = Path.GetFileName(file);
+                Console.WriteLine(GenerateComponent(filename));
+            }
+
+            foreach (var file in files)
+            {
+                var filename = Path.GetFileName(file);
+                Console.WriteLine(GenerateComponentRef(filename));
+            }
+
+        }
+
+        static string GetFileId(string filename) => string.Join("", filename.Split('.', '-'));
+
+        static string GenerateComponentRef(string filename) => $"<ComponentRef Id=\"{GetFileId(filename)}\" />";
+
+        static string GenerateComponent(string filename)
+        {
+            var fileid = GetFileId(filename);
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "WiXGen.WixComponentTemplate.xml";
+
+            var xmlTemplate = "";
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                xmlTemplate = reader.ReadToEnd();
+            }
+
+            return string.Format(xmlTemplate, fileid, filename);
         }
     }
 }
